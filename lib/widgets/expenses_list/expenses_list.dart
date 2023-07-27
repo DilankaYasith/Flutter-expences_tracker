a0b926c932a06759ffedec68fc9265cb3ffba820
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-07-27 23:49:03 +0530

    first commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,17 @@
+import 'package:expences_tracker/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+import 'package:expences_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (ctx, index) =>
+            ExpenseItem(expenses[index])); //scrollable list with unknown length
+  }
+}
