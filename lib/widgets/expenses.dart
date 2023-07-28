a96eeeebe7686528a4e712f8c1148d861a9fbe09
Dilanka Yasith <123799744+DilankaYasith@users.noreply.github.com>
2023-07-28 11:52:33 +0530

    expenses.dart updated

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,24 +28,69 @@ class _Expenses extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-        context: context, builder: (ctx) => const NewExpense());
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) => NewExpense(onAddExpense: _addExpense));
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense detected'),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
-      appBar: AppBar(title: const Text("Flutter ExpenseTracker"), actions: [
-        IconButton(
-          onPressed: _openAddExpenseOverlay,
-          icon: const Icon(Icons.add),
-        ),
-      ]),
+    Widget mainContent = const Center(
+      child: Text('No expenses fund. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            "Flutter ExpenseTracker",
+          ),
+          actions: [
+            IconButton(
+              onPressed: _openAddExpenseOverlay,
+              icon: const Icon(Icons.add),
+            ),
+          ]),
       body: Column(children: [
         const Text("The chart"),
         Expanded(
-          child: ExpensesList(expenses: _registeredExpenses),
+          child: mainContent,
         ),
       ]),
-    ));
+    );
   }
 }
